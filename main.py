6e81ab83bbc85724fd87a5ee8e0ad571be054982
Tristan Fillis <98738132+iightbulb@users.noreply.github.com>
2023-07-18 14:16:57 +0200

    changed recognition of input
    code now reads and acknowledges when user input is equal to the displayed word. word count and wpm also display correctly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import requests
+# GUI typing speedtest application using tkinter
+
+
 import tkinter as tk
+from tkinter import *
 import random
 
 
@@ -12,16 +15,17 @@ window.title("Test your typing speed")
 window.config(padx=60, pady=40, bg=CREAM)
 fg = BLACK
 
-word_site = "https://www.mit.edu/~ecprice/wordlist.10000"
-
-response = requests.get(word_site)
-WORDS = response.content.splitlines()
+word_list = []
+with open('words.txt', 'r') as f:
+    for line in f.readlines():
+        word_list.append(line.replace('\n', ''))
 
 val = tk.StringVar()
 user_entry = tk.Entry(window, textvariable=val)
 user_entry.grid(row=2, column=1, pady=50)
 user_entry.focus()
-words = tk.Label(window, text='Choose the duration of the test below. When ready, click Start.')
+words = tk.Label(window, text='Choose the duration of the test below. When ready, click Start.', bg=CREAM)
+words.config(font=('Arial', 20))
 words.grid(row=0, column=1)
 timer_text = tk.Label(window, text=30)
 timer_text.grid(row=4, column=1, pady=10)
@@ -46,11 +50,11 @@ def tksleep(t):
     window.wait_variable(var)
 
 
-def correct_word(*args):
-    if f" b'{val.get()}'" == words['text']:
+def correct_word():
+    if user_entry.get() == words['text']:
         print('delete user entry')
-        user_entry.delete(0)
-        words.config(text=random.choice(WORDS))
+        user_entry.delete(0, END)
+        words.config(text=random.choice(word_list))
         return True
     else:
         return False
@@ -58,19 +62,22 @@ def correct_word(*args):
 
 def start():
     total_time = int(timer_text['text'])
-    print(total_time)
-    words.config(text=random.choice(WORDS))
+    words.config(text=random.choice(word_list))
     i = 0
     word_count = 0
     while i < total_time:
-        if val.trace('w', correct_word):
+        if correct_word():
             word_count += 1
+            word_count_text = tk.Label(window, text=f"Word count: {word_count}")
+            word_count_text.grid(row=4, column=2)
         tksleep(1)
         i += 1
         timer_text['text'] = total_time - i
-        print(f"b'{val.get()}'")
+        print(f"b'{user_entry.get()}'")
         print(words['text'])
-    words.config(text=f"Time's up!You type at {int(word_count/total_time)} words per minute.")
+    print(word_count)
+    print(total_time)
+    words.config(text=f"Time's up!You type at {word_count/total_time*60} words per minute.")
 
 
 start_button = tk.Button(text="Start", highlightthickness=0, command=start)
